72201f68ee29c1d3697f21755a75da54db181006
nguyenthanhliem-ntl <nguyenthanhlimpk@gmail.com>
2021-08-27 10:57:59 +0700

    ok

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-import {BootstrapRoutingModule} from '../bootstrap/bootstrap-routing.module';
+import {CreateComponent} from './create/create.component';
+import {DeleteComponent} from './delete/delete.component';
+import {EditComponent} from './edit/edit.component';
+import {ListComponent} from './list/list.component';
+import {BookRoutingModule} from './book-routing.module';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateComponent,
+    DeleteComponent,
+    EditComponent,
+    ListComponent
+  ],
 
   imports: [
     CommonModule,
-   BootstrapRoutingModule
+    BookRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class BookModule { }
